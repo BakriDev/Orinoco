697f9954e5d364309890ac4aed46bbc273b92fd7
Bakri <toyb61@hotmail.com>
2021-05-21 10:23:32 +0200

    test site

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,33 +1,36 @@
 main()
 
 async function main() {
-  const article = await getArticles()
-  
-  for (article of articles) {
-    displayArticles(articles)
-  }
-    
+    const json = await getArticles()
+
+    for (const articles of json) {
+
+        displayArticles(articles)
+    }
+
 }
 
 function getArticles() {
-  return fetch("http://localhost:3000/api/cameras")
-  .then(function(httpBodyResponse) {
-    return httpBodyResponse.json()
-  })
-  .then(function(articles) {
-    return articles
-  })
-  .catch(function(error) {
-    alert(error)
-  })
+    return fetch("http://localhost:3000/api/cameras")
+        .then(function(httpBodyResponse) {
+            return httpBodyResponse.json()
+        })
+        .then(function(articles) {
+            return articles
+        })
+        .catch(function(error) {
+            alert(error)
+        })
 }
 
 function displayArticles(article) {
-  const templateElt = document.getElementById("templateArticle")
-  const clone = document.importNode(templateElt.content, true)
-  
-  cloneElt.getElementById("hbg").textContent = article.title
-  cloneElt.getElementById("hbg").textContent = article.body
-  
-  document.getElementById("main").appendChild(cloneElt)
+    const templateElt = document.getElementById("templateArticle")
+    const cloneElt = document.importNode(templateElt.content, true)
+
+    cloneElt.getElementById("article_url").href = article._id
+    cloneElt.getElementById("article_image").src = article.imageUrl
+    cloneElt.getElementById("article_name").textContent = article.name
+    cloneElt.getElementById("article_price").textContent = article.price
+
+    document.getElementById("main").appendChild(cloneElt)
 }
